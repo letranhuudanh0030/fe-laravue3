0f403f55839dc8b4dc34168dd3d7676d992fa8e0
DANH LE <letranhuudanh0030@gmail.com>
2022-02-08 11:54:54 +0700

    update baseURL api

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export default axios.create({
-    baseURL: 'https://be-laravue3.herokuapp.com/',
+    baseURL: 'http://be-laravue3.herokuapp.com/',
     timeout: 10000,
 });
